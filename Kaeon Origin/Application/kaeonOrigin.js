a58b7cd0af044e40bd9390d6d026d018a3e9af84
Jesse Dinkin <kaeon.ace@gmail.com>
2019-10-05 10:18:22 -0400

    Kaeon

--- Kaeon Origin/Application/kaeonOrigin.js
+++ Kaeon Origin/Application/kaeonOrigin.js
@@ -1,10 +1,23 @@
+var kaeonFUSION = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/United%20Bootstrap/KaeonFUSION.js");
+var onePlus = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/United%20Bootstrap/ONEPlus.js");			
+
 var ui = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/UI/UI.js")
 
-let text = ui.create("textarea");
+var text = ui.create("textarea");
 
 ui.extend(document.documentElement, text);
 
-let run = ui.create("button");
-run.innerHTML = "Run";
+var run = ui.create("button");
 
+run.innerHTML = "Run";
+run.onclick = onRun;
+
+ui.extend(document.documentElement, ui.create("br"));
 ui.extend(document.documentElement, run);
+
+function onRun() {
+
+	let fusion = new kaeonFUSION.KaeonFUSION();
+
+	fusion.process(onePlus.readONEPlus(text.value));
+}
