33a3720697860cf305ecda2f3c7f1325391fc482
Jesse Dinkin <kaeon.ace@gmail.com>
2019-10-05 12:39:24 -0400

    Kaeon

--- Kaeon Origin/Application/kaeonOrigin.js
+++ Kaeon Origin/Application/kaeonOrigin.js
@@ -1,23 +1,35 @@
+var one = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/Kaeon-FUSION/master/Kaeon%20FUSION/APIs/ONE/JavaScript/ONE.js");
 var kaeonFUSION = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/United%20Bootstrap/KaeonFUSION.js");
-var onePlus = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/United%20Bootstrap/ONEPlus.js");			
+var onePlus = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/United%20Bootstrap/ONEPlus.js");
 
-var ui = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/UI/UI.js")
+var ui = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/UI/UI.js");
 
 var text = ui.create("textarea");
 
 ui.extend(document.documentElement, text);
+ui.extend(document.documentElement, ui.create("br"));
 
 var run = ui.create("button");
 
 run.innerHTML = "Run";
 run.onclick = onRun;
 
-ui.extend(document.documentElement, ui.create("br"));
 ui.extend(document.documentElement, run);
+
+var show = ui.create("Show ONE");
+
+show.innerHTML = "Show ONE";
+show.onclick = showONE;
+
+ui.extend(document.documentElement, showONE);
 
 function onRun() {
 
 	let fusion = new kaeonFUSION.KaeonFUSION();
 
 	fusion.process(onePlus.readONEPlus(text.value));
+}
+
+function showONE() {
+	console.log(one.writeONE(onePlus.readONEPlus(text.value)));
 }
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Kaeon Origin is an online programming environment for Kaeon FUSION.
 
 The application can be accessed [here](https://gallery-of-kaeon.github.io?path=https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/United%20Bootstrap/index.html&unitedJS=https://raw.githubusercontent.com/Gallery-of-Kaeon/Kaeon-Origin/master/Kaeon%20Origin/Application/kaeonOrigin.js).
+
+Be sure to open the developer console of your browser when using it.
