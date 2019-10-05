e313845019052576f3b51e3a354ca0bf97fcb574
Jesse Dinkin <kaeon.ace@gmail.com>
2019-10-04 22:47:31 -0400

    Kaeon

--- Kaeon Origin/Application/kaeonOrigin.js
+++ Kaeon Origin/Application/kaeonOrigin.js
@@ -1,1 +1,10 @@
-console.log("Kaeon Origin");
+var ui = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/UI/UI.js")
+
+let text = ui.create("textarea");
+
+ui.extend(document.documentElement, text);
+
+let run = ui.create("button");
+run.innerHTML = "Run";
+
+ui.extend(document.documentElement, run);
