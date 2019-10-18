543e9ba7e3a9b9094a788a292c23f99edfc5cdbf
Jesse Dinkin <kaeon.ace@gmail.com>
2019-10-17 22:09:00 -0400

    Kaeon

--- Kaeon Origin/Application/kaeonOrigin.js
+++ Kaeon Origin/Application/kaeonOrigin.js
@@ -1,6 +1,4 @@
 var one = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/Kaeon-FUSION/master/Kaeon%20FUSION/APIs/ONE/JavaScript/ONE.js");
-var philosophersStone = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/Philosophers-Stone/master/Philosopher's%20Stone/API/JavaScript/PhilosophersStone.js");
-var fusion = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/United%20Bootstrap/FUSION.js");
 var kaeonFUSION = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/United%20Bootstrap/KaeonFUSION.js");
 var onePlus = require("https://raw.githubusercontent.com/Gallery-of-Kaeon/JavaScript-Utilities/master/JavaScript%20Utilities/United%20Bootstrap/ONEPlus.js");
 
@@ -41,8 +39,8 @@ ui.extend(document.documentElement, ui.create("br"));
 
 var run = ui.create("button");
 
-run.innerHTML = "Run";
-run.onclick = onRun;
+run.innerHTML = "Run Kaeon FUSION";
+run.onclick = onRunFUSION;
 
 ui.extend(document.documentElement, run);
 
@@ -54,7 +52,15 @@ show.onclick = showONE;
 ui.extend(document.documentElement, show);
 ui.extend(document.documentElement, ui.create("br"));
 
-var out = 
+var runJS = ui.create("button");
+
+runJS.innerHTML = "Run JavaScript";
+runJS.onclick = onRunJS;
+
+ui.extend(document.documentElement, runJS);
+ui.extend(document.documentElement, ui.create("br"));
+
+var out =
 	ui.setStyle(
 		ui.create("textarea"),
 		[
@@ -65,110 +71,91 @@ var out =
 out.readOnly = true;
 
 ui.extend(document.documentElement, out);
+ui.extend(document.documentElement, ui.create("br"));
 
-function consoleStone() {
+var display =
+	ui.setStyle(
+		ui.create("div"),
+		[
+			["width", "50vh"],
+			["height", "50vh"],
+			["border", "thick solid #000000"],
+			["overflow", "auto"]
+		]
+	);
 
-	philosophersStone.abide(this, new fusion.FUSIONUnit());
+ui.extend(document.documentElement, display);
 
-	var reference = this;
-
-	this.verify = function(element) {
-
-		if(!reference.cleared) {
-
-			try {
-
-				let literal =
-					philosophersStone.retrieve(
-						philosophersStone.traverse(reference),
-						function(item) {
-							return philosophersStone.isTagged(item, "Literal");
-						}
-					)[0];
-				
-				if(literal == null)
-					return false;
-
-				if(reference.fusion == null) {
-
-					reference.fusion =
-						philosophersStone.retrieve(
-							philosophersStone.traverse(reference),
-							function(item) {
-								return philosophersStone.isTagged(item, "FUSION");
-							}
-						)[0];
-				}
-
-				for(let i = 0; i < reference.fusion.fusionUnits.length; i++) {
-
-					if(reference.fusion.fusionUnits[i] == reference ||
-						philosophersStone.isTagged(reference.fusion.fusionUnits[i], "Literal") ||
-						philosophersStone.isTagged(reference.fusion.fusionUnits[i], "Variable") ||
-						philosophersStone.isTagged(reference.fusion.fusionUnits[i], "State") ||
-						philosophersStone.isTagged(reference.fusion.fusionUnits[i], "Priority")) {
-						
-						continue;
-					}
-
-					if(reference.fusion.fusionUnits[i].verify(one.createElement("Log")) ||
-						reference.fusion.fusionUnits[i].verify(one.createElement("Log Line")) ||
-						reference.fusion.fusionUnits[i].verify(one.createElement("Log Error")) ||
-						reference.fusion.fusionUnits[i].verify(one.createElement("Log Line Error")) ||
-						reference.fusion.fusionUnits[i].verify(one.createElement("Input"))) {
-
-						reference.fusion.fusionUnits[i].verify = function() { return false; };
-					}
-				}
-			}
-
-			catch(error) {
-
-			}
-
-			reference.cleared = true;
-		}
-
-		return element.content.toLowerCase() == "log" ||
-			element.content.toLowerCase() == "log line" ||
-			element.content.toLowerCase() == "log error" ||
-			element.content.toLowerCase() == "log line error" ||
-			element.content.toLowerCase() == "input";
-	}
-
-	this.process = function(element, processed) {
-
-		let data = "";
-
-		for(let i = 0; i < processed.length; i++)
-			data += "" + processed[i];
-
-		if(element.content.toLowerCase() != "input") {
-
-			out.value += data;
-
-			if(element.content.toLowerCase().startsWith("log line"))
-				out.value += "\n";
-		}
-
-		else
-			return prompt(data);
-
-		return null;
-	}
-}
-
-function onRun() {
-
-	let fusion = new kaeonFUSION.KaeonFUSION();
+function onRun(callback) {
 
 	out.value = "";
 
-	philosophersStone.connect(fusion, new consoleStone(), [], true);
+	let tempLog = console.log;
+	ui.root = display;
 
-	fusion.process(onePlus.readONEPlus(text.value));
+	console.log = function() {
+
+		for(let i = 0; i < arguments.length; i++)
+			out.value += "" + arguments[i] + " ";
+		
+		out.value += "\n";
+	}
+
+	readline = prompt;
+
+	ui.setStyle(
+		display,
+		[
+			["width", "50vh"],
+			["height", "50vh"],
+			["border", "thick solid #000000"],
+			["overflow", "auto"]
+		]
+	);
+
+	display.innerHTML = "";
+
+	tempDoc = display;
+
+	try {
+		callback();
+	}
+
+	catch(error) {
+		out.value = "ERROR";
+	}
+
+	console.log = tempLog;
+	ui.root = document.documentElement;
+}
+
+function onRunFUSION() {
+
+	onRun(
+		function() {
+
+			(new kaeonFUSION.KaeonFUSION()).process(
+				onePlus.readONEPlus(text.value));
+		}
+	);
+}
+
+function onRunJS() {
+
+	onRun(
+		function() {
+			eval(text.value);
+		}
+	);
 }
 
 function showONE() {
-	out.value = one.writeONE(onePlus.readONEPlus(text.value));
+
+	try {
+		out.value = one.writeONE(onePlus.readONEPlus(text.value));
+	}
+
+	catch(error) {
+		out.value = "ERROR: INVALID ONE+";
+	}
 }
